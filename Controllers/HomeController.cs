d870737a4b2be6e59afc4644f1c25486beeabae5
Katlego Makhuba <128791831+st10366193@users.noreply.github.com>
2025-09-29 10:43:40 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ViewBag_age_urlPass.Models;
+
+namespace ViewBag_age_urlPass.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int age)
+        {
+            ViewBag.user = age;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewBag_age_urlPass.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
